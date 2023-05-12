d2d357119b5f34811ca482331b606b44bfba8c77
janahani <Jana2100237@miuegypt.edu.eg>
2023-05-12 22:22:00 +0200

    packages+app.js

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ const PORT=8080;
 const indexRoute=require('./routes/Indexroute.js');
 const bodyParser = require('body-parser');
 const{check,validationResult}=require('express-validator');
+const fs=require('fs');
+const addActivity=require('./routes/AddActivityRoute');
+const Activity= require('./models/addActivitiesdb.js');
+var db = mongoose.connection;
+var multer=require('multer');
+const cors=require('cors');
+const fileupload=require("express-fileupload");
+
+app.use(cors());
+app.use(fileupload());
+
+const storage = multer.diskStorage({
+  destination:function(req,file,callback){
+    cb(null,__dirname+'uploads');
+  },
+  filename: function(req,file,cb){
+    cb(null,Date.now()+'--'+file.originalname);
+  }
+});
+
+const uploads = multer({storage:storage});
 
 const urlencodedParser=bodyParser.urlencoded({ extended: false });
 
@@ -29,45 +50,43 @@ mongoose.connect(dburl,{ useNewUrlParser: true, useUnifiedTopology: true })
 
 
 app.use("/",indexRoute);
+app.use("/",indexRoute);
+app.use("/success",indexRoute);
+app.use('/AddActivity',function(req,res)
+{
+    res.render("AddActivity");
+});
 
-app.post('/',urlencodedParser,[
-  check('unam','Username must be 3+ characters long')
-  .exists()
-  .isLength({min:3})
-  ,
-
-  check('email','Email is not valid')
-  .isEmail()
-  .normalizeEmail()
-
-] ,(request, response) =>  {
+app.post('/submit',fileupload(),(request, response) =>  {
   console.log("entered");
-
-  const errors=validationResult(request)
-  if(!errors.isEmpty()){
-      const alert=errors.array();
-      response.render('index',{alert});
-  }
- else{
-    const userdetails = new User({
-        Username: request.body.unam,
-        Email: request.body.email,
-        PhoneNumber: request.body.number,
-        Password: request.body.psw,
-        ConfPassword: request.body.confpsw,
+    const activitydetails = new Activity({
+        Name:request.body.Aname,
+        Type:request.body.Atype,
+        Picture:{
+          data:request.file,
+          contentType:'image/png'
+        },
+        BriefDes:request.body.Abrief,
+        DetailedDes:request.body.Adetails,
+        Plan:request.body.Aplan,
+        CancelDet:request.body.Acancel,
+        Duration:request.body.Atime,
+        PickupDet:request.body.Apickup,
+        AvailableDate:request.body.Adate
       });
-    userdetails.save()
-    .then((result)=>
-    {
-        console.log("saved");
-       // response.redirect('/');
-    })
-    .catch((err) =>
-    {
-      console.log(err);
-    });
-  }
-  });
+      console.log(request.files);
+    db.collection("activities").insertOne(activitydetails,(err,result)=>{
+      if(err)
+      {
+       console.log(err);
+      }
+       console.log("saved");
+       response.redirect('/');
+
+    
+  })
+});
+
 
 
     
--- routes/Indexroute.js
+++ routes/Indexroute.js
@@ -12,15 +12,32 @@ router.get('/',function(req,res)
 
 module.exports=router;
 
-router.post('/success', (request, response) =>  {
-    console.log("entered");
-      const userdetails = new User({
-          Username: request.body.unam,
-          Email: request.body.email,
-          PhoneNumber: request.body.number,
-          Password: request.body.psw,
-          ConfPassword: request.body.confpsw,
-        });
+router.post('/',urlencodedParser,[
+  check('unam','Username must be 3+ characters long')
+  .exists()
+  .isLength({min:3})
+  ,
+
+  check('email','Email is not valid')
+  .isEmail()
+  .normalizeEmail()
+
+] ,(request, response) =>  {
+  console.log("entered");
+
+  const errors=validationResult(request)
+  if(!errors.isEmpty()){
+      const alert=errors.array();
+      response.render('index',{alert});
+  }
+ else{
+    const userdetails = new User({
+        Username: request.body.unam,
+        Email: request.body.email,
+        PhoneNumber: request.body.number,
+        Password: request.body.psw,
+        ConfPassword: request.body.confpsw,
+      });
       db.collection("users").insertOne(userdetails,(err,result)=>{
         if(err)
         {
@@ -30,7 +47,6 @@ router.post('/success', (request, response) =>  {
          console.log("saved");
          response.redirect('/');
         }
-  
-      
-    })
+  })
+ }
   });

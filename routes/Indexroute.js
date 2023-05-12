889039f7af9dd9b72df019057f6fcf7e468d34c9
janahani <Jana2100237@miuegypt.edu.eg>
2023-05-12 22:09:24 +0200

    routing

--- routes/Indexroute.js
+++ routes/Indexroute.js
@@ -1,5 +1,9 @@
 const express=require('express')
 const router=express.Router();
+const User= require('../models/usersdb.js');
+const mongoose=require('mongoose');
+var db = mongoose.connection;
+
 
 router.get('/',function(req,res)
 {
@@ -7,3 +11,26 @@ router.get('/',function(req,res)
 });
 
 module.exports=router;
+
+router.post('/success', (request, response) =>  {
+    console.log("entered");
+      const userdetails = new User({
+          Username: request.body.unam,
+          Email: request.body.email,
+          PhoneNumber: request.body.number,
+          Password: request.body.psw,
+          ConfPassword: request.body.confpsw,
+        });
+      db.collection("users").insertOne(userdetails,(err,result)=>{
+        if(err)
+        {
+         console.log(err);
+        }
+        else{
+         console.log("saved");
+         response.redirect('/');
+        }
+  
+      
+    })
+  });

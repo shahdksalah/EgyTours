56237b3aa906839cf055c30efa0acd0f4739398f
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-05-14 23:33:46 +0300

    scrolljs

---
+++ public/js/scrolll.js
@@ -0,0 +1,17 @@
+function scrolll(){
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(-350,0);
+}
+function scrolllr(){
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(350,0);
+}
+
+function scrolll2(){
+    var left1 = document.querySelector(".scroll-images1");
+    left1.scrollBy(-350,0);
+}
+function scrolllr2(){
+    var right1 = document.querySelector(".scroll-images1");
+    right1.scrollBy(350,0);
+}

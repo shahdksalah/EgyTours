a7b82e96806bf71c1d955a363c02693611604b6b
janahani <Jana2100237@miuegypt.edu.eg>
2023-05-17 01:47:29 +0200

    edit activities

--- routes/editactivitiesroute.js
+++ routes/editactivitiesroute.js
@@ -1,10 +1,19 @@
 const express=require('express')
 const router=express.Router()
+const validate =require('../public/js/formsVal.js');
+const Activity =require('../models/addActivitiesdb.js');
+const mongoose =require('mongoose');
+var db = mongoose.connection;
+const bodyParser=require('body-parser');
+const{check,validationResult} =require('express-validator');
+const urlencodedParser=bodyParser.urlencoded({ extended: false });
 
-
-router.get('/',function(req,res)
+router.get('/',async function(req,res)
 {
-    res.render("EditActivities");
+    var Activities=[];
+    Activities=await Activity.find();
+    console.log(Activities)
+    res.render("EditActivities",{activities:(Activities==='undefined'?"":Activities)});
 });
 
 module.exports=router;

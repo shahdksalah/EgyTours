993deb468cf0dc7f75528ee202844106558af3ea
shahd <shahd2106627@miuegypt.edu.eg>
2023-05-17 15:16:38 +0300

    hotels route edit

--- routes/hotelsroute.js
+++ routes/hotelsroute.js
@@ -7,8 +7,14 @@ router.get('/', async function(req,res)
 {
     var Hotels=[];
     Hotels=await Hotel.find();
-    console.log(Hotels);
     res.render("hotels",{hotels:(Hotels==='undefined'?"":Hotels)});
+});
+
+router.get('/:name', async function(req,res){
+    var Hotels=[];
+    var url = req.params.name;
+    Hotels=await Hotel.find({"Name":url});
+    res.render("hotel1",{hotel1:(Hotels==='undefined'?"":Hotels)});
 });
 
 module.exports=router;

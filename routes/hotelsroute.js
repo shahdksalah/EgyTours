f600453cec45f3a6bfb401818acc325ddd8254b0
janahani <Jana2100237@miuegypt.edu.eg>
2023-05-17 18:19:51 +0200

    hotel

--- routes/hotelsroute.js
+++ routes/hotelsroute.js
@@ -17,8 +17,34 @@ router.get('/:name', async function(req,res){
     res.render("hotel1",{hotel1:(Hotels==='undefined'?"":Hotels)});
 });
 
-router.post('/',async function(req,res){
+router.post('/:name',async function(req,res){
+    console.log(req.body.rating);
     console.log(req.body.addrev);
+    console.log(req.params.name);
+
+    var arr=[];
+    arr[0]=req.body.addrev;
+    arr[1]=req.body.rating;
+    arr[2]=new Date();
+
+    
+    var Hotels=[];
+    var query1=req.body.hotel;
+    const hotel1=await Hotel.find().where("Name").equals(query1);
+    Hotels=Array.from(hotel1);
+    console.log(hotel1);
+    const filter={Name:query1};
+    const update={Reviews:arr};
+    if(hotel1!=='undefined')
+    {
+        await Hotel.findOneAndUpdate(filter,update)
+        .then(result =>
+            {
+                console.log(result);
+            });
+    
+    }  
+
 });
 
 module.exports=router;

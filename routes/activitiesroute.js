773a918ada36032312da96bfd87d15625c9ffc32
janahani <Jana2100237@miuegypt.edu.eg>
2023-05-19 17:17:37 +0200

    activity route

--- routes/activitiesroute.js
+++ routes/activitiesroute.js
@@ -2,9 +2,19 @@ const express=require('express')
 const router=express.Router()
 const Activity = require('../models/addActivitiesdb.js');
 
-router.get('/',function(req,res)
+router.get('/',async function(req,res)
 {
-    res.render("activities");
+    var Activities=[];
+    Activities=await Activity.find();
+    res.render("activities",{activities:(Activities==='undefined'?"":Activities)});
 });
+
+router.get('/:name', async function(req,res){
+    var Activities=[];
+    var url = req.params.name;
+    Activities=await Activity.find({"Name":url});
+    res.render("activity1",{activity1:(Activities==='undefined'?"":Activities)});
+});
+
 
 module.exports=router;

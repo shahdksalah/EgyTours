582488ab21f8f77db5923a5ee6a35805428be4ac
mennaemam12 <mennaemam205@gmail.com>
2023-05-22 20:03:14 +0300

    .

--- public/js/editprofile.js
+++ public/js/editprofile.js
@@ -1,23 +1,3 @@
-const users =[
-    {"id": "1", "name":"shahd", "email":"shahd@gmail.com", "age": 20,"number":"01023234", "password":"1234"},
-    {"id": "2", "name":"menna", "email":"menna@gmail.com", "age": 19,"number":"01029213","password":"123"},
-    {"id": "3", "name":"laila", "email":"laila@gmail.com", "age": 19,"number":"01029903","password":"12345"},
-    {"id": "4", "name":"jana", "email":"jana@gmail.com", "age": 19,"number":"010234553","password":"123456"},
-]
-var usN = document.getElementById("uname");
-var usE = document.getElementById("femail");
-var usNo = document.getElementById("phonum");
-var usA = document.getElementById("age");
-
-function displayInfo(email){
-    var user = users.find((val,idx,arr) => {return val.email = email});
-    if(user){
-        usN.value = user.name;
-        usE.value = user.email;
-        usNo.value= user.number;
-        usA.value = user.age;
-    }
-}
 
 function editProf(){
     var doc = document.getElementById("editForm");
@@ -44,29 +24,4 @@ function cancel(){
 
 
 
-var us = document.getElementById("user");
-var usr = document.getElementById("username");
-var use = document.getElementById("uemail");
-var usp = document.getElementById("uphone");
-var usa = document.getElementById("uage");
-function displayName(email){
-    var user = users.find((val,idx,arr) => {return val.email = email});
-    if(user){
-        us.innerHTML = user.name;
-        usr.innerHTML = user.name;
-        use.innerHTML = user.email;
-        usp.innerHTML = user.number;
-        usa.innerHTML = user.age;
-    }
-}
 
-
-
-window.onload = function(){
-   displayName('shahd@gmail.com');
-}
-
-
-function saveInfo(){
-
-}

261f5196f9ef467e4ffb1aec1b33173bdd8bd56f
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-05-25 20:00:09 +0300

    .

---
+++ models/addcitiesdb.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { trim } = require('lodash');
+const app = express()
+const mongoose = require('mongoose');
+
+const Schema=mongoose.Schema;
+app.use(express.urlencoded({extended:true}));
+
+const CitySchema= new Schema({
+    Name:{
+        type:String,
+        trim:true,
+        required:true,
+    },
+    picture:{
+        type:Array,
+        required:true,
+    }
+
+
+},{timestamp:true});
+const Cities= mongoose.model('Cities',CitySchema);
+module.exports=Cities;

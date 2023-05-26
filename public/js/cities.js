1a5aa5bdd76681836aac3cda0824366c44fbe8bc
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-05-26 14:53:15 +0300

    .

--- public/js/cities.js
+++ public/js/cities.js
@@ -4,6 +4,7 @@ form.addEventListener('submit',Event=>{
 
         var selected=new Array();
         var hotels=document.getElementsByName("hot");
+        var activities1=document.getElementsByName("act");
         for(var i=0;i<hotels.length;i++)
         {
             if(hotels[i].checked){
@@ -11,6 +12,13 @@ form.addEventListener('submit',Event=>{
             }
         }
         document.getElementById("selectedHotels").innerText=selected;
-    
+
+        for(var j=0;j<activities1.length;j++)
+        {
+            if(activities1[j].checked){
+                selected.push(activities1[j].value);
+            }
+        }
+        document.getElementById("selectedActivities").innerText=selected;
     
 });

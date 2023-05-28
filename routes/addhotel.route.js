36f42c51d4e69bc8d0ff6ff491815561d5b21c90
mennaemam12 <mennaemam205@gmail.com>
2023-05-28 20:32:04 +0300

    .

--- routes/addhotel.route.js
+++ routes/addhotel.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Hotel = require('../models/hotel.schema.js');
 const fileUpload = require('express-fileupload');
-
+const bodyParser=require('body-parser');
 router.use(bodyParser.json());
 
 router.use((req, res, next) => {

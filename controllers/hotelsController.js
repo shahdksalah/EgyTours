d5c71a7a588e971d9a0e9a017349915c6835e70b
mennaemam12 <mennaemam205@gmail.com>
2023-05-29 18:36:51 +0300

    .

--- controllers/hotelsController.js
+++ controllers/hotelsController.js
@@ -1,18 +1,58 @@
 const express=require('express')
-const router=express.Router()
-const Hotel = require('../models/hotel.schema.js');
+const router=express.Router();
 const Cart=require('../models/cartdb.js');
 
 const addToCart=async(req,res)=>{
     console.log('Entered')
-   const hotel= new Cart.hotelBooking(req.session.user.Username,req.body.checkIn,req.body.checkOut,
-    req.body.adults,req.body.children,req.body.rooms);
-    const cart= new Cart.Cart(req.session.user._id,hotel);
-    cart.save()
-    .then(result=>{
-        console.log("Hotel added");
-        res.redirect('back')
+    
+
+
+    var hotels=[];
+
+    var Hotel={
+        id:req.params.id,
+        checkIn:req.body.checkIn,
+        checkOut:req.body.checkOut,
+        children:req.body.children,
+        adults:req.body.adults,
+        rooms:req.body.rooms,
+        roomType:req.body.roomType,
+    }
+    
+    var query = { Userid: req.session.user._id};
+    Cart.find(query)
+    .then( async result=>{
+        var crt=result[0];
+       if(crt){
+        hotels=result[0].Hotels;
+        hotels.push(Hotel);
+        
+        await Cart.findByIdAndUpdate(result[0]._id, {
+            Hotels: hotels
+        })
+        .then(result=>{
+            res.redirect('back');
+        })
+
+       }
+       else{
+          hotels[0]=Hotel;
+          if(req.session.user){
+            const cart= new Cart({
+                Userid:req.session.user._id,
+                Hotels:hotels,
+            });
+            cart.save()
+            .then(result=>{
+                console.log("Hotel added");
+                res.redirect('back')
+            })
+        }
+       }
     })
+     
+
+  
 }
 
 module.exports={addToCart};

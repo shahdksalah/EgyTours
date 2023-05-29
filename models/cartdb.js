f7c93aeaa38d1b572ce730bb162e6652786cb831
mennaemam12 <mennaemam205@gmail.com>
2023-05-29 03:20:50 +0300

    cart

---
+++ models/cartdb.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+app.use(express.urlencoded({extended:true}));
+
+class hotelBooking{
+    constructor(checkIn,checkOut,adults,children,rooms){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.adults = adults;
+        this.children = children;
+        this.rooms = rooms;
+    }
+}
+
+const cartSchema=new Schema({
+    Userid:{
+        type:String,
+        unique:true,
+        trim:true,
+        required:true,
+    },
+    hotels:{
+        type:Array,
+    }
+},{timestamp:true});
+
+const Cart= mongoose.model('Cart',cartSchema);
+module.exports=Cart,hotelBooking;

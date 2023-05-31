5c191b98e5442c2a96378adcfd5c055adbffbedc
mennaemam12 <mennaemam205@gmail.com>
2023-06-01 02:13:50 +0300

    .

--- routes/paymentroute.js
+++ routes/paymentroute.js
@@ -4,6 +4,27 @@ const paymentController=require('../controllers/payment.controller.js')
 const { check, validationResult } = require('express-validator');
 
 router.get('/', paymentController.viewForm);
-router.post('/payment',paymentController.pay)
+router.post('/payment', urlencodedParser, [
+    check('number', 'Card Number Required')
+      .exists()
+    ,
+  
+    check('expMonth', 'Expiry Month Required')
+    .exists(),
+    
+  
+    check('expDay', 'Expiry Day Required')
+    .exists(),
+  
+    check('psw', 'Invalid Password')
+      .exists()
+      .isLength({ min: 6 }),
+  
+    check('confpsw', 'Invalid Password')
+      .exists()
+      .isLength({ min: 6 })
+  
+  
+  ],paymentController.pay)
 
 module.exports=router;

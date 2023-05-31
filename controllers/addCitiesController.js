edf351123ac62353af72c7ac993f00c01ca692dd
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-06-01 00:39:13 +0300

    .

--- controllers/addCitiesController.js
+++ controllers/addCitiesController.js
@@ -1,60 +1,63 @@
-const express=require('express')
-const router=express.Router()
-const fileupload=require("express-fileupload");
-const path = require('path');   
-const Cities = require('../models/addcitiesdb');
-const Hotelss =require('../models/hotel.schema.js');
-const Activityy =require('../models/activity.schema.js');
-const bodyParser=require('body-parser');
-const CityController=require('../controllers/addCitiesController.js')
+const express = require("express");
+const router = express.Router();
+const fileupload = require("express-fileupload");
+const path = require("path");
+const Cities = require("../models/addcitiesdb");
+const Hotelss = require("../models/hotel.schema.js");
+const Activityy = require("../models/activity.schema.js");
+const bodyParser = require("body-parser");
+const CityController = require("../controllers/addCitiesController.js");
 
 router.use(bodyParser.json());
 
-
 router.use(fileupload());
 
-
 const getActandHotels = async (req, res) => {
-    var arr=[];
-    arr=await Hotelss.find();
-    var arr2=[];
-    arr2=await Activityy.find();
-    res.render("addcities",{hotels:arr,activities:arr2});
-}
+  var arr = [];
+  arr = await Hotelss.find();
+  var arr2 = [];
+  arr2 = await Activityy.find();
+  res.render("addcities", { hotels: arr, activities: arr2 });
+};
 
+const postCities = async (request, response) => {
+  console.log("entered");
+  var imgFile;
+  var uploadPath;
+  var ext;
+  if (!request.files || Object.keys(request.files).length === 0) {
+    return response.status(400).send("no files uploaded");
+  }
 
-const postCities = async (req, res) => {
-    console.log("entered");
-    var imgFile;
-    var uploadPath;
-    var ext;
-    if(!request.files||Object.keys(request.files).length===0){
-      return response.status(400).send("no files uploaded");
-    }
+  imgFile = request.files.imgs;
+  var paths = [];
+  ext = imgFile.name.split(".")[1];
+  uploadPath =
+    __dirname +
+    "/../public/images/cities/" +
+    request.body.cityname +
+    1 +
+    "." +
+    ext;
+  imgFile.mv(uploadPath);
+  paths[0] = request.body.cityname + "." + ext;
 
-    imgFile=request.files.imgs;
-    var paths = [];
-      ext = imgFile.name.split('.')[1];
-      uploadPath=__dirname+'/../public/images/cities/'+ request.body.cityname+ (1) + '.' + ext;
-      imgFile.mv(uploadPath);
-      paths[0]=request.body.cityname+'.'+ext;
+  console.log(request.body);
+  const city = new Cities({
+    Name: request.body.cityname,
+    picture: paths[0],
+    hotels: request.body.hotels,
+    activities: request.body.activities1,
+  });
 
-    console.log(request.body);
-    const city=new Cities({
-        Name:request.body.cityname,
-        picture:paths[0],
-        hotels:request.body.hotels,
-        activities:request.body.activities1
+  city
+    .save()
+    .then((result) => {
+      response.redirect("/addcities");
+    })
+    .catch((err) => {
+      console.log(err);
     });
+};
 
-    city.save()
-        .then(result=>{
-            response.redirect('/addcities');
-        })
-        .catch(err=>{
-            console.log(err);
-        })      
-}
-
-
-module.exports = { getActandHotels,postCities };
+module.exports = { getActandHotels, postCities };
--- public/js/cities.js
+++ public/js/cities.js
@@ -1,30 +1,22 @@
-var form=document.getElementById("form");
+var form = document.getElementById("form");
 
-form.addEventListener('submit',Event=>{
+form.addEventListener("submit", (Event) => {
+  var selected = new Array();
+  var selected1 = new Array();
+  var hotels = document.getElementsByName("hot");
+  var activities1 = document.getElementsByName("act");
 
-        var selected=new Array();
-        var selected1=new Array();
-        var hotels=document.getElementsByName("hot");
-        var activities1=document.getElementsByName("act");
+  for (var i = 0; i < hotels.length; i++) {
+    if (hotels[i].checked) {
+      selected.push(hotels[i].value);
+    }
+  }
+  document.getElementById("selectedHotels").innerText = selected;
 
-        for(var i=0;i<hotels.length;i++)
-        {
-            if(hotels[i].checked){
-                selected.push(hotels[i].value);
-            }
-        }
-        document.getElementById("selectedHotels").innerText=selected;
-
-
-
-
-        for(var j=0;j<activities1.length;j++)
-        {
-            if(activities1[j].checked){
-                selected1.push(activities1[j].value);
-            }
-        }
-        document.getElementById("selectedActivities").innerText=selected1;
-    
+  for (var j = 0; j < activities1.length; j++) {
+    if (activities1[j].checked) {
+      selected1.push(activities1[j].value);
+    }
+  }
+  document.getElementById("selectedActivities").innerText = selected1;
 });
-

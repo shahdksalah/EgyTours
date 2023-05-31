0e3e22625e652a9018e0846e48a536d63508cd7b
mennaemam12 <mennaemam205@gmail.com>
2023-06-01 02:07:06 +0300

    .

--- routes/paymentroute.js
+++ routes/paymentroute.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const router=express.Router();
 const paymentController=require('../controllers/payment.controller.js')
+const { check, validationResult } = require('express-validator');
 
 router.get('/', paymentController.viewForm);
 router.post('/payment',paymentController.pay)

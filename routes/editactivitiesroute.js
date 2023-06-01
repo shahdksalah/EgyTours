83d74011483e6ff022583db89d6c04fe038b6fa3
janahani <Jana2100237@miuegypt.edu.eg>
2023-06-01 20:55:52 +0200

    edit activities

--- routes/editactivitiesroute.js
+++ routes/editactivitiesroute.js
@@ -36,6 +36,28 @@ router.use(fileUpload());
 
 router.post('/updated/:name', async function (req, res) {
 
+    var date=req.body.alldates.length;
+    var dates=req.body.alldates;
+    var arr=[];
+    var arr2=[];
+    var count=0;
+    for(var j=0;j<date;j++)
+    {
+      if(dates[j]===","){
+        count++;
+      }
+    }
+    console.log(count);
+    for(var k =0;k<count;k++)
+    {
+      arr.push(dates.split(',')[k]+" "+ "0");
+    }
+
+    for(var s=0;s<arr.length;s++)
+    {
+        console.log(arr[s]);
+    }
+  
 
     var Activities = [];
 
@@ -47,7 +69,7 @@ router.post('/updated/:name', async function (req, res) {
         console.log("hi");
     }
     else {
-        
+
         var filenames = Object.keys(req.files);
         var imgFile;
         var searchpath;
@@ -60,32 +82,33 @@ router.post('/updated/:name', async function (req, res) {
         })
     }
 
-    //   await Activity.findByIdAndUpdate(req.body.id,{
-    //     Name:request.body.Cityname,
-    //     Header:request.body.Activityname,
-    //     Days:request.body.Days,
-    //     Type:request.body.ActivityType,
-    //     Rate:request.body.rate,
-    //     Picture:paths,
-    //     Advantage:request.body.ActivityAdv,
-    //     BriefDes:request.body.Activitybrief,
-    //     DetailedDes:request.body.Activitydet,
-    //     Plan:request.body.Activityplan,
-    //     CancelDet:request.body.Activitycancel,
-    //     Duration:request.body.Activitydur,
-    //     PickupDet:request.body.Activitypickup,
-    //     Starttime:request.body.Activitystart,
-    //     Endtime:request.body.Activityend,
-    //     Price:request.body.Activityprice,
-    //     //DatesDetails:arr,
-    //     MaxParticipants:request.body.Activitymax
-    //   })
-    //   .then(()=>{
-    //    
-    //     });
-    //   })
-    //res.render("editactivity1", { activity: (Activities === 'undefined' ? "" : Activities) });
+    await Activity.findByIdAndUpdate(req.body.id, {
+        Name: req.body.Cityname,
+        Header: req.body.Activityname,
+        Days: req.body.Days,
+        Type: req.body.ActivityType,
+        Rate: req.body.rate,
+        Advantage: req.body.ActivityAdv,
+        BriefDes: req.body.Activitybrief,
+        DetailedDes: req.body.Activitydet,
+        Plan: req.body.Activityplan,
+        CancelDet: req.body.Activitycancel,
+        Duration: req.body.Activitydur,
+        PickupDet: req.body.Activitypickup,
+        Starttime: req.body.Activitystart,
+        Endtime: req.body.Activityend,
+        Price: req.body.Activityprice,
+        DatesDetails: arr,
+        MaxParticipants: req.body.Activitymax
+    })
+
+        .then(async result => {
+            Activities = await Activity.find().where("Name").equals(query)
+                .then(() => {
+                    res.render("activity1", { activity1: (Activities === 'undefined' ? "" : Activities), user: (!req.session.authenticated) ? "" : req.session.user, msg: "" });
+                })
+        })
+
 });
 
-
-    module.exports = router;
+module.exports = router;

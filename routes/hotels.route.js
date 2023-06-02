f34f08c5425d2d4339ca486eb7c1c65b9e0e379f
shahd <shahd2106627@miuegypt.edu.eg>
2023-06-02 16:43:18 +0300

    .

--- routes/hotels.route.js
+++ routes/hotels.route.js
@@ -1,16 +1,20 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express')
+const router = express.Router()
 const Hotel = require('../models/hotel.schema.js');
-const hotelController=require('../controllers/hotelsController.js')
+const Activity = require('../models/activity.schema.js');
+const hotelController = require('../controllers/hotelsController.js')
 
 
 
-router.get('/',hotelController.getHotels);
+router.get('/', hotelController.getHotels);
 
 router.get('/:name', hotelController.getHotel1);
 
 router.post('/:name', hotelController.postReview);
-router.post('/:name/:id',hotelController.addToCart)
+
+router.post('/:name/:id', hotelController.addToCart);
 
 
-module.exports=router;
+
+
+module.exports = router;

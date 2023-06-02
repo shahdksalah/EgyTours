986f3f07c7d17c4c5239e23ffc7a6d679b6f656f
mennaemam12 <mennaemam205@gmail.com>
2023-06-02 13:00:47 +0300

    .

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,59 +3,60 @@ const Hotel = require('../models/hotel.schema.js');
 const Activity = require('../models/activity.schema.js');
 
 const viewCart = async function (req, res) {
-  var empty=false;
-  let cart;
-  let hotels = [];
-  let activities=[];
+  var empty = false;
+  var cart;
+  var hotels = [];
+  var activities = [];
   var length;
   var counter = 0;
   if (req.session.authenticated) {    //user signed in
     console.log("authenticated")
-    const promise1=new Promise(async (resolve,reject)=>{
-      resolve(
-        await Cart.find().where("User").equals(req.session.user._id)  //user has items in cart
+    const promise1 = new Promise(async (resolve, reject) => {
+
+      await Cart.find().where("User").equals(req.session.user._id)  //user has items in cart
         .then(result => {
-          if(result.length>1){
-          hasItems=true;
-          cart = result[0];
-           length = cart.Hotels.length+cart.Activities.length;
-          cart.Hotels.forEach(async hotel => {
-  
-            await Hotel.find().where("_id").equals(hotel.id)
-              .then(resu => {
-                if (resu.length > 0) {
-                  console.log("hotel added");
-                  hotels.push(resu[0]);
-                  counter++;
-                }
+          if (result.length > 1) {
+            hasItems = true;
+            cart = result[0];
+            length = cart.Hotels.length + cart.Activities.length;
+            resolve(
+              cart.Hotels.forEach(async hotel => {
+
+                await Hotel.find().where("_id").equals(hotel.id)
+                  .then(resu => {
+                    if (resu.length > 0) {
+                      console.log("hotel added");
+                      hotels.push(resu[0]);
+                      counter++;
+                    }
+                  })
+                  .catch(err => {
+                    console.log(err);
+                  })
               })
-              .catch(err=>{
-                console.log(err);
-              })
-          })
+            )
           }
+          else{
+            resolve();
+          }
+          // else{          
+          //   console.log("entered here") ;        //user signed in but doesn't have items in cart
+          //   res.render("cart", {
+          //     user: (!req.session.authenticated) ? "" : req.session.user,
+          //     cart: "", hotels: "", activities: ""
+          //   })
+          // }
         })
-        // .catch(err => {    user signed in but doesn't have items in cart
-        //    empty=true;
-        // })    
-      )
-      reject(
-          //user not signed in
-          res.render("cart", {
-            user: (!req.session.authenticated) ? "" : req.session.user,
-            cart: "", hotels: "",activities:""
-          })
-    
-      )
-     
-       
-      
+      .catch(err => {    
+            console.log(err);
+      })    
+
     })
 
-    promise1.then(()=>{
-      
-        cart.Activities.forEach(async activity=>{
-          await Activity.find().where("_id").equals(activity.id)  
+    promise1.then(() => {
+       console.log(cart.Activities);
+      cart.Activities.forEach(async activity => {
+        await Activity.find().where("_id").equals(activity.id)
           .then(resul => {
             if (resul.length > 0) {
               console.log("activity added");
@@ -66,23 +67,24 @@ const viewCart = async function (req, res) {
                 console.log(hotels);
                 res.render("cart", {
                   user: (!req.session.authenticated) ? "" : req.session.user,
-                  cart: cart, hotels: hotels,activities:activities
+                  cart: cart, hotels: hotels, activities: activities
                 });
               }
             }
           })
-          .catch(err=>{
+          .catch(err => {
             console.log(err);
           })
-        })
+      })
     })
-  } 
-  else{
+  }
+  else {
+    console.log("entered final else")
     res.render("cart", {
       user: (!req.session.authenticated) ? "" : req.session.user,
-      cart: "", hotels: "",activities:""
+      cart: "", hotels: "", activities: ""
     })
-  }    
+  }
 }
 
 module.exports = { viewCart };

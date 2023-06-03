28c30b58892d0917a26c2ad6172548ab9a50a61f
shahd <shahd2106627@miuegypt.edu.eg>
2023-06-03 03:26:25 +0300

    ajax search edit + login

---
+++ public/js/ajax-LOGIN.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    $('#loginbut').on('click', function (e) {
+        e.preventDefault();
+        var data = $("#username").val();
+        var data1 = $("#password1").val();
+            $.ajax({
+                url: '/login',
+                method: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify({ Username: data, Password: data1 }),
+                success: function (response) {
+                    console.log(response);
+                    if (response==="up") {
+                        $('#un').html("Username cannot be blank");
+                        $('#pass').html("Password cannot be blank");
+                    }
+                    else if (response==="u") {
+                        $('#un').html("");
+                        $('#un').html("Username cannot be blank");
+                    }
+                    else if (response==="p") {
+                        $('#un').html("");
+                        $('#pass').html("Password cannot be blank");
+                    }
+                    else if(response==="invalid"){
+                        $('#un').html("");
+                        $('#pass').html("Password is not correct");
+                    }
+                    else if(response==="upinvalid"){
+                        $('#un').html("Username is invalid");
+                        $('#pass').html("Password is invalid");
+                    }
+                    else if(response==="uinvalid"){
+                        $('#un').html("Username is invalid");
+                        $('#pass').html("");
+                    }
+                    else if(response==='pinvalid'){
+                        $('#un').html("");
+                        $('#pass').html("Password is invalid");
+                    }
+                    else if(response==="not found"){
+                        $('#un').html("");
+                        $('#pass').html("This account does not exist");
+                    }
+                    else{
+                        $('#un').html("");
+                        $('#pass').html("");
+                        $("body").html(response);
+                    }
+
+                },
+                error: function (err) {
+
+                }
+            });
+        
+    })
+})

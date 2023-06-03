1d1782e46fea334c3549cdad79d518b0c6cb0350
shahd <shahd2106627@miuegypt.edu.eg>
2023-06-03 19:32:31 +0300

    payment route unfinished

--- routes/paymentroute.js
+++ routes/paymentroute.js
@@ -1,22 +1,12 @@
 const express=require('express')
 const router=express.Router();
-const paymentController=require('../controllers/payment.controller.js')
+const {viewForm, validateCard,pay}=require('../controllers/payment.controller.js')
 const { check} = require('express-validator');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get('/', paymentController.viewForm);
-router.post('/payment', urlencodedParser, [
-    check('number', 'Card Number Required')
-      .exists()
-    ,
-  
-    check('cvv', 'CVV Required')
-      .exists(),
-  
-    check('name', 'Card Holder Name Required')
-      .exists()
-  
-  ],paymentController.pay)
+router.get('/', viewForm);
+
+router.post('/payment',validateCard(), pay)
 
 module.exports=router;

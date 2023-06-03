ce176e6eb24ff6c019a922968d8deae53ce82ed6
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-06-04 01:40:57 +0300

    chatrouts

---
+++ routes/chatroute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const Chat = require("../models/chatdb");
+router.get("/", function (req, res) {
+  res.render("chat", {
+    user: !req.session.authenticated ? "" : req.session.user,
+  });
+});
+
+router.post("/submit", (req, res) => {
+  const chat = new Chat({
+    senderName: req.body.mydata.name,
+    message: req.body.mydata.message,
+  });
+  chat
+    .save()
+    .then((result) => {
+      response.redirect("/chat");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;

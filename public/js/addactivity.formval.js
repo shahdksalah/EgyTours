11f2344dba0c0545c295b3dd8aacf6e0c5be599e
janahani <Jana2100237@miuegypt.edu.eg>
2023-06-03 12:23:51 +0200

    formVal

--- public/js/addactivity.formval.js
+++ public/js/addactivity.formval.js
@@ -1,5 +1,43 @@
 var form = document.getElementById("form");
 form.addEventListener('submit', (e) => {
+    var error = false;
+    var name = document.getElementById('ActName');
+    var header = document.getElementById('ActHeader');
+    var type = document.getElementById('ActType');
+    var imgs = document.getElementById('imgs');
+    var adv = document.getElementById('adv');
+    var brief = document.getElementById('Actbrief');
+    var details = document.getElementById('Actdetails');
+    var plan = document.getElementById('ActPlan');
+    var cancel = document.getElementById('ActCancel');
+    var duration = document.getElementById('ActTime');
+    var pickup = document.getElementById('ActPickup');
+    var max = document.getElementById('ActPart');
+    var start = document.getElementById('starttime');
+    var end = document.getElementById('endtime');
+    var price = document.getElementById('price');
+    var availdates = document.getElementById('ActDates');
+
+    if (name.value === "") {
+        document.getElementById('error-name').innerHTML = "You must enter city's name";
+        error = true;
+    }
+
+    if (header.value === "") {
+        document.getElementById('error-header').innerHTML = "You must enter activity's name";
+        error = true;
+    }
+
+    if (type.value === "") {
+        document.getElementById('error-type').innerHTML = "You must enter activity's type";
+        error = true;
+    }
+
+    if (imgs.value === "") {
+        document.getElementById('error-header').innerHTML = "You must upload 3 images";
+        error = true;
+    }
+
 
 
 

bd715fdd5ab92f13a470b93ef03baeb4b6241c03
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-06-04 01:40:35 +0300

    .

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,48 +1,69 @@
-let count=0;
-let clone;
-let textfield = document.getElementById('msg');
-        
-function sendMsg(){
-    const d = new Date();
-    let text = document.getElementById('msg').value;
-    textfield.value="";
-    if(count%2==0){
-        clone=duplicate('left-message', count);
-        clone.querySelector('.textmsg').innerHTML = text;
-        clone.style.display="flex";
-        count++;
-    }
-    else{
-        clone=duplicate('right-message',count);
-        clone.querySelector('.textmsg').innerHTML = text;
-        clone.style.display="flex";
-        count++;
-    }
-        
+const socket = io();
+
+const messageContainer = document.getElementById("message-container");
+
+const nameInput = document.getElementById("name-input");
+
+const messageForm = document.getElementById("message-form");
+
+const messageInput = document.getElementById("message-input");
+
+var type = document.getElementById("usertype");
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendMessage();
+});
+
+function sendMessage() {
+  if (messageInput.value === "") return;
+  console.log(messageInput.value);
+  var name;
+  if (type.value === "admin") {
+    name = "admin";
+  } else {
+    name = nameInput.innerHTML;
+  }
+  const data = {
+    name: nameInput.innerHTML,
+    message: messageInput.value,
+    dateTime: new Date(),
+  };
+
+  $.ajax({
+    url: "chat/submit",
+    method: "POST",
+    data: { mydata: data },
+    success: function (response) {},
+  });
+  socket.emit("message", data);
+  addMessagetoUI(true, data);
+  messageInput.value = "";
 }
 
+//for receiving a message
+socket.on("chat-message", (data) => {
+  console.log(data);
+  addMessagetoUI(false, data);
+});
 
-function duplicate(id, i) {
-    let original = document.getElementById(id);
-    let clone = original.cloneNode(true); // "deep" clone
-    clone.id = "msg" + ++i;
-    // or clone.id = ""; if the divs don't need an ID
-    original.parentNode.appendChild(clone);
-    return clone;
-        }
-
-let c = 0;
-function viewChat(id){
-    let chat = document.getElementById(id);
-    if(c%2==0){
-        chat.classList.add('popup');
-        chat.style.display='block';
-        c++;
-    }
-    else{
-        chat.classList.remove('popup');
-        chat.style.display='none';
-        c++;
-    }
+function addMessagetoUI(isOwnMessage, data) {
+  const element = `
+                    <li class="${
+                      isOwnMessage ? "message-right" : "message-left"
+                    }">
+                    <p class="message">
+                        ${data.message}
+                        <span>
+                        ${data.name} ${moment(data.dateTime).fromNow()}
+                        </span>
+                    </p>
+                    </li>
+                `;
+  messageContainer.innerHTML += element;
+  scrollToBottom();
 }
 
+function scrollToBottom() {
+  messageContainer.scrollTo(0, messageContainer.scrollHeight);
+}

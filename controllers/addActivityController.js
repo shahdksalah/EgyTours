6417312d9cee1c72dc997cf1d3065db67001076d
janahani <Jana2100237@miuegypt.edu.eg>
2023-06-03 10:38:30 +0200

    update add activity

--- controllers/addActivityController.js
+++ controllers/addActivityController.js
@@ -1,14 +1,14 @@
-const express=require('express')
-const router=express.Router()
-const Activity= require('../models/activity.schema.js');
-const fileupload=require("express-fileupload");
-const bodyParser=require('body-parser');
+const express = require('express')
+const router = express.Router()
+const Activity = require('../models/activity.schema.js');
+const fileupload = require("express-fileupload");
+const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(fileupload());
 
 
 const getAddActivity = async (req, res) => {
-    res.render("AddActivity");
+  res.render("AddActivity");
 }
 
 
@@ -18,69 +18,71 @@ const postAddActivity = async (req, res) => {
   var uploadPath;
   var num;
   var ext;
-  if(!req.files||Object.keys(req.files).length===0){
+  if (!req.files || Object.keys(req.files).length === 0) {
     return response.status(400).send("no files uploaded");
   }
-  num=req.files.imgs.length;
-  imgFile=req.files.imgs;
+  num = req.files.imgs.length;
+  imgFile = req.files.imgs;
   var paths = [];
-  for(var i=0;i<num;i++){
+  for (var i = 0; i < num; i++) {
     ext = imgFile[i].name.split('.')[1];
-    uploadPath=__dirname+'/../public/images/activities/'+ req.body.Aname + (i+1) + '.' + ext;
+    uploadPath = __dirname + '/../public/images/activities/' + req.body.Aname + (i + 1) + '.' + ext;
     imgFile[i].mv(uploadPath);
-    paths[i]=req.body.Aname+(i+1)+'.'+ext;
+    paths[i] = req.body.Aname + (i + 1) + '.' + ext;
   }
-  
 
-  var date=req.body.Dates.length;
-  var dates=req.body.Dates;
-  var arr=[];
-  var arr2=[];
-  var count=0;
-  for(var j=0;j<date;j++)
-  {
-    if(dates[j]===","){
+
+  
+  var date = req.body.Dates.length;
+  var dates = req.body.Dates;
+  var count = 0;
+  for (var j = 0; j < date; j++) {
+    if (dates[j] === ",") {
       count++;
     }
   }
-  console.log(count);
-  for(var k =0;k<= count;k++)
-  {
-    arr.push(dates.split(',')[k]+" "+ "0");
-  }
 
+  var alldates=[];
+  var begin=0;
+  for (var i = 0; i <= count; i++) {
+    var newdates = {
+      date: req.body.Dates.split(',')[i],
+      max: begin
+    }
+    alldates.push(newdates);
+  }
 
   const activitydetails = new Activity({
-    Name:req.body.Aname,
-    Header:req.body.Aheader,
-    Type:req.body.Atype,
-    Rate:req.body.rate,
-    Picture:paths,
-    Advantage:req.body.adv,
-    BriefDes:req.body.Abrief,
-    DetailedDes:req.body.Adetails,
-    Plan:req.body.Aplan,
-    CancelDet:req.body.Acancel,
-    Duration:req.body.Atime,
-    PickupDet:req.body.Apickup,
-    Starttime:req.body.starttime,
-    Endtime:req.body.endtime,
-    Price:req.body.price,
-    DatesDetails:arr,
-    MaxParticipants:req.body.Aparticipants
-    });
+    Name: req.body.Aname,
+    Header: req.body.Aheader,
+    Type: req.body.Atype,
+    Rate: req.body.rate,
+    Picture: paths,
+    Advantage: req.body.adv,
+    BriefDes: req.body.Abrief,
+    DetailedDes: req.body.Adetails,
+    Plan: req.body.Aplan,
+    CancelDet: req.body.Acancel,
+    Duration: req.body.Atime,
+    PickupDet: req.body.Apickup,
+    Starttime: req.body.starttime,
+    Endtime: req.body.endtime,
+    Price: req.body.price,
+    DatesDetails: alldates,
+    MaxParticipants: req.body.Aparticipants
+  });
 
-    activitydetails.save()
-      .then(result=>{
-        console.log(result);
-        res.redirect('/activities');
-      })
-      .catch(err=>{
-        console.log(err);
-      })
+  activitydetails.save()
+    .then(result => {
+      console.log(result);
+      res.redirect('/activities');
+    })
+    .catch(err => {
+      console.log(err);
+    })
 
 
 
-  }
+}
 
-  module.exports = { getAddActivity,postAddActivity };
+module.exports = { getAddActivity, postAddActivity };

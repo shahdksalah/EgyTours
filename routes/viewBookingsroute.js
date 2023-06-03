b4ffa9f72bec08baa9f7f012cc4474f487cc235d
janahani <Jana2100237@miuegypt.edu.eg>
2023-06-03 19:29:16 +0200

    .

--- routes/viewBookingsroute.js
+++ routes/viewBookingsroute.js
@@ -5,6 +5,8 @@ router.use(bodyParser.json());
 const fileUpload = require('express-fileupload');
 const Booking= require('../models/bookingdb.js');
 const User=require("../models/usersdb.js");
+const Hotel= require('../models/hotel.schema.js');
+const Activity=require("../models/activity.schema.js");
 
 router.use(fileUpload());
 
@@ -23,8 +25,13 @@ router.get('/', async function (req, res) {
     Bookings = await Booking.find();
     var Users = [];
     Users = await User.find();
+    var Hotels = [];
+    Hotels = await Hotel.find();
+    var Activities = [];
+    Activities = await Activity.find();
     res.render("viewbookings", { bookings: (Bookings === 'undefined' ? "" : Bookings) },
-    { users:(Users === 'undefined' ? "" : Users)});
+    { users:(Users === 'undefined' ? "" : Users)}, { hotels:(Hotels === 'undefined' ? "" : Hotels)},
+    { activities:(Activities === 'undefined' ? "" : Activities)});
 });
 
 module.exports = router;

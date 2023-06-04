cee7fb4ec64bdae5f8d7075d396cf617145ad78a
shahd <shahd2106627@miuegypt.edu.eg>
2023-06-05 01:54:16 +0300

    .

--- routes/adminboardroute.js
+++ routes/adminboardroute.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser');
 router.use(bodyParser.json());
-const Hotel = require('../models/hotel.schema.js');
-const Activity =require('../models/activity.schema.js');
 const dashboard=require('../controllers/admindashboardController.js');
 
 router.use((req, res, next) => {

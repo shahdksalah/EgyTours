e6c9939b27b333a5d9be9263bc914cd29eaf8ac9
mennaemam12 <mennaemam205@gmail.com>
2023-06-04 22:49:15 +0300

    .

--- public/js/cart.js
+++ public/js/cart.js
@@ -2,34 +2,18 @@ function hideItem(id){
     let item = document.getElementById(id);
     item.style.display="none";
 
-    var data=id
-    $.ajax({
-            url: 'cart/removeBooking',
-            method: 'POST',
-            data:{sentId:data},
-            success:function(response){
-                // if(response=="empty"){
-                //     $('.r').css("display", "none");
-                //     $('#empty').css("display", "block");
-                // }
-                // else if(response.split(' ')[0]=="success"){
-                    
-                //     console.log(response.split(' ')[1])   
-                //     var t=$('#total').html();
-                //     console.log(total);
-                //     var t1=parseInt(t)-parseInt(response.split(' ')[1]);
-                //     var count=$('#cnt').html();
-                //     count--;
-                //     $('#cnt').html(count);
-                //     $('#total').html(t1);
-
-                    
-                // }
+    // var data=id
+    // $.ajax({
+    //         url: 'cart/removeBooking',
+    //         method: 'POST',
+    //         data:{sentId:data},
+    //         success:function(response){
+           
                 
-                    $("body").html(response);
+    //                 $("body").html(response);
                 
-            }
-    })
+    //         }
+    // })
      
 }
 

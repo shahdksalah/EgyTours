f0bfac4e52ef49c2900d03e81025fbcee73cd6b7
shahd <shahd2106627@miuegypt.edu.eg>
2023-06-04 19:34:38 +0300

    js edit

--- public/js/popupJS.js
+++ public/js/popupJS.js
@@ -1,25 +1,25 @@
 
 
-function show(id){
+function show(id) {
     let signin = document.getElementById(id);
-    signin.style.display='block';
+    signin.style.display = 'block';
 
-    document.getElementById('username').value="";
-    document.getElementById('password1').value="";
+    document.getElementById('username').value = "";
+    document.getElementById('password1').value = "";
 
-    document.getElementById('un').innerHTML="";
-    document.getElementById('pass').innerHTML="";
+    document.getElementById('un').innerHTML = "";
+    document.getElementById('pass').innerHTML = "";
 }
-function hide(id){
+function hide(id) {
     let x = document.getElementById(id);
-    x.style.display='none';
+    x.style.display = 'none';
 }
 
-function hide2(id1,id2){
+function hide2(id1, id2) {
     let x = document.getElementById(id1);
     let y = document.getElementById(id2);
-    x.style.display='none';
-    y.style.display='none';
+    x.style.display = 'none';
+    y.style.display = 'none';
 
     var errors = document.getElementsByClassName('error-message');
     for (let index = 0; index < errors.length; index++) {
@@ -29,21 +29,21 @@ function hide2(id1,id2){
 
 }
 
-
-    
-
-async function handleLogout()  {
-    location.href = "http://localhost:8080/signout";
+function showdrop() {
+    document.getElementById("drop").style.display = 'block';
 
 }
+function hidedrop() {
+    document.getElementById("drop").style.display = "none";
+}
 
- function displayDropdown(){
-     document.getElementById("dropdown-content").style.display="block";
- }
+function displayDropdown() {
+    document.getElementById("dropdown-content").style.display = "block";
+}
 
- function hideDropdown(){
-     document.getElementById("dropdown-content").style.display="none";
- }
+function hideDropdown() {
+    document.getElementById("dropdown-content").style.display = "none";
+}
 
-      
+
 

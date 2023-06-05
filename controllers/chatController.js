ac91ae6b46230edef6dc0e9d3d18c7df119808ca
mennaemam12 <mennaemam205@gmail.com>
2023-06-06 02:44:04 +0300

    .

---
+++ controllers/chatController.js
@@ -0,0 +1,19 @@
+const Chat = require("../models/chatdb");
+
+const saveChat=async(req,res)=>{
+    try{
+        var chat=new Chat({
+            sender_id:req.body.sender_id,
+            receiver_id:req.body.receiver_id,
+            message:req.body.message
+        });
+
+        await chat.save();
+        res.status(200).send({success:true,msg:'Chat inserted!'});
+    }
+    catch(error){
+        res.status(400).send({succes:false,msg:error.message});
+    }
+}
+
+module.exports={saveChat};

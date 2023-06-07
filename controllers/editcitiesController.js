ef66509a6c32d6d940aaafea1385fea3471fc06a
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-06-07 03:04:18 +0300

    .

--- controllers/editcitiesController.js
+++ controllers/editcitiesController.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const City = require("../models/addcitiesdb");
 const fileUpload = require("express-fileupload");
+const path = require("path");
+const fs = require("fs");
 
 router.use(fileUpload());
 
@@ -12,15 +14,51 @@ const getCities = async (req, res) => {
 };
 
 const updateCity = async (req, res) => {
-  await City.findByIdAndUpdate(req.body.id, {
-    Name: req.body.imgname,
+  let imgFile;
+  let uploadPath;
+  console.log(req.files);
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  imgFile = req.files.img;
+  uploadPath = path.join(
+    __dirname,
+    "../public/images/cities" + req.body.id + path.extname(imgFile.name)
+  );
+
+  imgFile.mv(uploadPath, async function (err) {
+    if (err) res.status(500).send(err);
+    const city = await City.findByIdAndUpdate(req.body.id, {
+      Name: req.body.imgname,
+      picture: req.body.image + path.extname(imgFile.name),
+    });
+    city
+      .save()
+      .then((result) => {
+        res.redirect("editcities");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
-  res.redirect("editcities");
 };
 
 const deleteCity = async (req, res) => {
-  var cit = await City.findByIdAndDelete(req.params.id);
-  if (cit) res.redirect("editcities");
+  await City.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      fs.unlink(
+        path.join(__dirname, "../public/images/cities/" + req.params.pic),
+        (err) => {
+          if (err) {
+            throw err;
+          }
+          res.redirect("back");
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 module.exports = { getCities, deleteCity, updateCity };

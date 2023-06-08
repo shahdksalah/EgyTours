6b086d929883c00d58c2b572b803cd9e4ad643e5
mennaemam12 <mennaemam205@gmail.com>
2023-06-08 23:14:11 +0300

    .

--- app.js
+++ app.js
@@ -133,3 +133,16 @@ app.use("/wishlist", wishlistRoute);
 app.use((req,res)=>{
   res.status(404).render('404',{user:(req.session.user === undefined ? "":req.session.user) });
 });
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+  // render the error page
+  res.status(err.status || 500);
+  res.render('404');
+});
+
+//console.log("ENV: ", app.get('env'));
+module.exports= app;
